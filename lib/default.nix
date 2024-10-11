6a0494f09aa9815e3f625ed82728ae96732ccb79
Zach Bedewi <zbbedewi@amazon.com>
2024-10-11 18:42:31 -0500

    Adding library functions.

---
+++ lib/default.nix
@@ -0,0 +1,16 @@
+{lib, ...}: {
+  relativeToRoot = lib.path.append ../.;
+  scanPaths = path:
+    builtins.map (f: (path + "/${f}")) (
+      builtins.attrNames (
+        lib.attrsets.filterAttrs (
+          path: _type:
+            (_type == "directory")
+            || (
+              (path != "default.nix")
+              && (lib.strings.hasSuffix ".nix" path)
+            )
+        ) (builtins.readDir path)
+      )
+    );
+}
