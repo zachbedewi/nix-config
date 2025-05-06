458eb50a144e0eb126bb31bf00292ebb89a509bd
Zach Bedewi <zachary.bedewi@protonmail.com>
2025-05-06 14:47:22 -0500

    Updating custom lib documentation

--- lib/default.nix
+++ lib/default.nix
@@ -1,14 +1,16 @@
 {lib, ...}: {
+  # Use path relative to the root of the project
   relativeToRoot = lib.path.append ../.;
+
   scanPaths = path:
     builtins.map (f: (path + "/${f}")) (
       builtins.attrNames (
         lib.attrsets.filterAttrs (
           path: _type:
-            (_type == "directory")
+            (_type == "directory") # include directories
             || (
-              (path != "default.nix")
-              && (lib.strings.hasSuffix ".nix" path)
+              (path != "default.nix") # ignore default.nix
+              && (lib.strings.hasSuffix ".nix" path) # include .nix files
             )
         ) (builtins.readDir path)
       )
